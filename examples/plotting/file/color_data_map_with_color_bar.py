4d26b3692b0da2ad384b010315c0eba5ecc404bb
Sarah Bird <sbird@continuum.io>
2016-08-18 01:55:23 -0700

    Use Spectral10

--- examples/plotting/file/color_data_map_with_color_bar.py
+++ examples/plotting/file/color_data_map_with_color_bar.py
@@ -2,23 +2,25 @@ import numpy as np
 
 from bokeh.io import show
 from bokeh.models import ColumnDataSource, LinearColorMapper, ColorBar
-from bokeh.palettes import Viridis3
+from bokeh.palettes import Spectral10
 from bokeh.plotting import figure
 
-x = np.random.random(2500) * 10
+x = np.random.random(2500) * 100
 y = np.random.normal(size=2500) * 2 + 5
 source = ColumnDataSource(dict(x=x, y=y))
 
-lin_mapper = LinearColorMapper(palette=Viridis3, low=0, high=10)
+mapper = LinearColorMapper(palette=Spectral10, low=0, high=100)
 color_bar = ColorBar(
-    color_mapper=lin_mapper,
-    legend_width='auto',
-    legend_height='auto'
+    orientation='horizontal',
+    color_mapper=mapper,
+    legend_width=500,
+    legend_height='auto',
+    location='center',
 )
 
 p = figure()
 opts = dict(x='x', line_color=None, source=source)
-p.circle(y='y', fill_color={'field': 'x', 'transform': lin_mapper}, **opts)
+p.circle(y='y', fill_color={'field': 'x', 'transform': mapper}, **opts)
 p.add_layout(color_bar)
 
 show(p)
