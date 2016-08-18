01824dda2e129fc95504dadd1cc18a4561e58273
Sarah Bird <sbird@continuum.io>
2016-08-18 02:55:14 -0700

    Update example

--- examples/plotting/file/color_data_map_with_color_bar.py
+++ examples/plotting/file/color_data_map_with_color_bar.py
@@ -2,25 +2,19 @@ import numpy as np
 
 from bokeh.io import show
 from bokeh.models import ColumnDataSource, LinearColorMapper, ColorBar
-from bokeh.palettes import Spectral10
+from bokeh.palettes import Viridis3
 from bokeh.plotting import figure
 
 x = np.random.random(2500) * 100
 y = np.random.normal(size=2500) * 2 + 5
 source = ColumnDataSource(dict(x=x, y=y))
 
-mapper = LinearColorMapper(palette=Spectral10, low=0, high=100)
-color_bar = ColorBar(
-    orientation='horizontal',
-    color_mapper=mapper,
-    legend_width=500,
-    legend_height='auto',
-    location='center',
-)
+mapper = LinearColorMapper(palette=Viridis3, low=0, high=100)
+color_bar = ColorBar(color_mapper=mapper, location=(0,0))
 
-p = figure()
+p = figure(toolbar_location='above')
 opts = dict(x='x', line_color=None, source=source)
 p.circle(y='y', fill_color={'field': 'x', 'transform': mapper}, **opts)
-p.add_layout(color_bar)
+p.add_layout(color_bar, 'right')
 
 show(p)
